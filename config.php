055ef2ff4887c59329c04d021a0e91f9364302a6
dodiitt <dodhie.virgo4@gmail.com>
2018-10-25 16:19:52 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+$android_id = "5fea5b8b0d3c7865";
+$deviceCode = "359998061389187";
+$device_ip  = "36.84.0.102";
+$token = "872cZPQUxewRAEy2pfAlDvw-qZOL_v1n3a5mLRfwVhEDVH5Wh1KitDWnqEXbfD7BnDqChrRlXY8YZeY";
+$tk = "ACAhOxJsNchEbqZ59zAIXmq5lWbGN3wA08FxdHRodw";
+$uuid = "8579c13a3f424336b399547d77161ad3";
+$userid = "3310566";
+$sign = "ddd013bd33ac1f41f89bed31f673e772";
+
+include(readline("masukan config: "));
