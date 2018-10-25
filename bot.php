3ea61feda166c42eafb4d1dc75f37bfca0db1432
dodiitt <dodhie.virgo4@gmail.com>
2018-10-25 16:39:53 +0700

    Create bot.php

---
+++ bot.php
@@ -0,0 +1,1063 @@
+/* Decoded by Jack'n'Jill */
+
+<?php $ {
+
+    "GLOBALS"
+
+}
+
+["efmtknc"] = "poin";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["pukkqlfcjtm"] = "ijo";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["nhyqmucofj"] = "read";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["szsvckknw"] = "js";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["pgrdzv"] = "result";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["lysnlqtckvq"] = "sign";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["lzhknrtizcqw"] = "uuid";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["ffdybgjl"] = "token";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["ptpnieffk"] = "tk";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["qleqqtbk"] = "device_ip";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["ianmuxkq"] = "android_id";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["cesjsivis"] = "link";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["cmdxmrdhhj"] = "ch";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["dqylyoyholy"] = "id";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["egnqlgpyv"] = "artikel";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["rjwrvhmzig"] = "rnd";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["pgqjxrdfvnv"] = "timeIn30Minutes";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["fepdpe"] = "putih";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["kmhdndncmcv"] = "turkis";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["qfjxhbraqo"] = "biru";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["ywuoiuu"] = "pink";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["mlemzrdds"] = "t";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["jokbog"] = "r";
+
+$hmtzcywicdlk = "red";
+
+@system("clear");
+
+$ {
+
+    $ {
+
+        "GLOBALS"
+
+    }
+
+    ["mlemzrdds"]
+
+} = "
+
+";
+
+$ {
+
+    $ {
+
+        "GLOBALS"
+
+    }
+
+    ["jokbog"]
+
+} = "
+
+";
+
+$axylqxtxv = "kuning";
+
+require_once ("config.php");
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["ogvsedgvegv"] = "kuning";
+
+$eznpgglixex = "t";
+
+require __DIR__ . "/modul/login.php";
+
+require __DIR__ . "/modul/absen.php";
+
+require __DIR__ . "/modul/cid.php";
+
+$ {
+
+    "GLOBALS"
+
+}
+
+["ujkipxmqgxy"] = "red";
+
+require __DIR__ . "/modul/share.php";
+
+$geggvcgsc = "ijo";
+
+$ {
+
+    $ {
+
+        "GLOBALS"
+
+    }
+
+    ["qfjxhbraqo"]
+
+} = "[1;34m";
+
+$ {
+
+    $ {
+
+        "GLOBALS"
+
+    }
+
+    ["kmhdndncmcv"]
+
+} = "[1;36m";
+
+$ {
+
+    $geggvcgsc
+
+} = "[92m";
+
+$ {
+
+    $ {
+
+        "GLOBALS"
+
+    }
+
+    ["fepdpe"]
+
+} = "[1;37m";
+
+$nmlkfxo = "t";
+
+$ {
+
+    $ {
+
+        "GLOBALS"
+
+    }
+
+    ["ywuoiuu"]
+
+} = "[1;35m";
+
+$ {
+
+    $hmtzcywicdlk
+
+} = "[1;31m";
+
+$ {
+
+    $axylqxtxv
+
+} = "[1;33m";
+
+echo $ {
+
+    $ {
+
+        "GLOBALS"
+
+    }
+
+    ["ogvsedgvegv"]
+
+} . "[>] membaca artikel………" . $ {
+
+    $eznpgglixex
+
+};
+
+echo $ {
+
+    $ {
+
+        "GLOBALS"
+
+    }
+
+    ["ujkipxmqgxy"]
+
+} . "Jeda (30 Detik)" . $ {
+
+    $nmlkfxo
+
+};
+
+sleep(3);
+
+while (true) {
+
+    $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["pgqjxrdfvnv"]
+
+    } = time() + 30 * 60;
+
+    $kdgknoeykos = "userid";
+
+    $kaqkrpvnh = "js";
+
+    $ {
+
+        "GLOBALS"
+
+    }
+
+    ["heghbgjjbt"] = "result";
+
+    $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["rjwrvhmzig"]
+
+    } = array_rand($ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["egnqlgpyv"]
+
+    });
+
+    $ {
+
+        "GLOBALS"
+
+    }
+
+    ["mettnmtyj"] = "link";
+
+    $cuhtfgwfgv = "ch";
+
+    $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["dqylyoyholy"]
+
+    } = $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["egnqlgpyv"]
+
+    }
+
+    [$ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["rjwrvhmzig"]
+
+    }
+
+    ];
+
+    $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["cmdxmrdhhj"]
+
+    } = curl_init();
+
+    $edwfydv = "body";
+
+    $vilggcvrdiq = "ch";
+
+    $udejvknxvz = "ch";
+
+    $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["cesjsivis"]
+
+    } = "http://api.beritaqu.net/timing/read";
+
+    $votent = "poin";
+
+    $gkxxrelcv = "id";
+
+    curl_setopt($ {
+
+        $vilggcvrdiq
+
+    }, CURLOPT_URL, $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["mettnmtyj"]
+
+    });
+
+    curl_setopt($ {
+
+        $udejvknxvz
+
+    }, CURLOPT_RETURNTRANSFER, 1);
+
+    $wdejckm = "poin";
+
+    $ {
+
+        "GLOBALS"
+
+    }
+
+    ["ltpkjrfnw"] = "poin";
+
+    $ {
+
+        "GLOBALS"
+
+    }
+
+    ["ebglsqo"] = "body";
+
+    $iltnbhos = "deviceCode";
+
+    $ytfjpqpqnxm = "msg";
+
+    curl_setopt($ {
+
+        $cuhtfgwfgv
+
+    }, CURLOPT_SSL_VERIFYPEER, 0);
+
+    curl_setopt($ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["cmdxmrdhhj"]
+
+    }, CURLOPT_TIMEOUT, 3600);
+
+    curl_setopt($ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["cmdxmrdhhj"]
+
+    }, CURLOPT_POST, true);
+
+    $ {
+
+        $edwfydv
+
+    } = "OSVersion=OSVersion=7.&android_channel=google&android_id=" . $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["ianmuxkq"]
+
+    } . "&content_id=" . $ {
+
+        $gkxxrelcv
+
+    } . "&content_type=1&deviceCode=" . $ {
+
+        $iltnbhos
+
+    } . "&device_brand=Xiaomi&device_ip=" . $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["qleqqtbk"]
+
+    } . "&device_version=Mi-4c&dtu=001&lat=-6.8854977&lon=107.6228489&network=wifi&pack_channel=google&time=" . $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["pgqjxrdfvnv"]
+
+    } . "&tk=" . $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["ptpnieffk"]
+
+    } . "&token=" . $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["ffdybgjl"]
+
+    } . "&uuid=" . $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["lzhknrtizcqw"]
+
+    } . "&userid=" . $ {
+
+        $kdgknoeykos
+
+    } . "&version=10055&versionName=1.5.5&sign=" . $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["lysnlqtckvq"]
+
+    };
+
+    curl_setopt($ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["cmdxmrdhhj"]
+
+    }, CURLOPT_POSTFIELDS, $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["ebglsqo"]
+
+    });
+
+    $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["heghbgjjbt"]
+
+    } = curl_exec($ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["cmdxmrdhhj"]
+
+    });
+
+    curl_close($ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["cmdxmrdhhj"]
+
+    });
+
+    $ {
+
+        $kaqkrpvnh
+
+    } = json_decode($ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["pgrdzv"]
+
+    }, true);
+
+    $ {
+
+        $ytfjpqpqnxm
+
+    } = $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["szsvckknw"]
+
+    }
+
+    ["message"];
+
+    $ {
+
+        $votent
+
+    } = $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["szsvckknw"]
+
+    }
+
+    ["data"]["amount"];
+
+    $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["nhyqmucofj"]
+
+    } = $ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["szsvckknw"]
+
+    }
+
+    ["data"]["current_read_second"];
+
+    if ($ {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["ltpkjrfnw"]
+
+    } >= 1) {
+
+        $ebuevg = "ijo";
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["ixvgxvh"] = "turkis";
+
+        $svshkiwl = "putih";
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["rjlvhm"] = "msg";
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["jeutgqrgvxx"] = "putih";
+
+        echo $ {
+
+            $svshkiwl
+
+        } . "[+]Message : " . $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["pukkqlfcjtm"]
+
+        } . $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["rjlvhm"]
+
+        } . $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["jeutgqrgvxx"]
+
+        } . " | Poin : " . $ {
+
+            $ebuevg
+
+        } . $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["efmtknc"]
+
+        } . $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["fepdpe"]
+
+        } . " | Read Second : " . $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["ixvgxvh"]
+
+        } . $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["nhyqmucofj"]
+
+        } . $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["mlemzrdds"]
+
+        };
+
+    } elseif ($ {
+
+        $wdejckm
+
+    } == 0) {
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["pqosgvjhpdko"] = "poin";
+
+        $mlrmykeqs = "ijo";
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["cykgipndcb"] = "t";
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["ojhzbzedk"] = "putih";
+
+        $bqdhxiymto = "red";
+
+        echo $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["ojhzbzedk"]
+
+        } . "poin: " . $ {
+
+            $mlrmykeqs
+
+        } . $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["pqosgvjhpdko"]
+
+        } . $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["cykgipndcb"]
+
+        };
+
+        $ {
+
+            "GLOBALS"
+
+        }
+
+        ["wuloccetyfdc"] = "kuning";
+
+        echo $ {
+
+            $bqdhxiymto
+
+        } . "warning !!!" . $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["mlemzrdds"]
+
+        } . "[!]" . $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["fepdpe"]
+
+        } . "Message :  " . $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["wuloccetyfdc"]
+
+        } . "jatah poin harian ente sudah habis" . $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["mlemzrdds"]
+
+        } . $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["kmhdndncmcv"]
+
+        } . "Ingat ! Nuyulnya Jangan Serakah " . $ {
+
+            $ {
+
+                "GLOBALS"
+
+            }
+
+            ["mlemzrdds"]
+
+        };
+
+        break;
+
+        exit;
+
+    }
+
+    sleep(30);
+
+}
+
+echo "
+
+";
+
+?>
